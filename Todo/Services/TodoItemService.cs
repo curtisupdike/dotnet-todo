327e08614bec5251e12fb039c0d4f166eccf83c7
Curtis Updike <cjupdike@icloud.com>
2022-06-26 15:11:17 -0400

    Connect to db

---
+++ Todo/Services/TodoItemService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Todo.Data;
+using Todo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Todo.Services
+{
+	public class TodoItemService : ITodoItemService
+	{
+		private readonly ApplicationDbContext _context;
+
+		public TodoItemService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+			return await _context.Items
+				.Where(x => x.IsDone == false)
+				.ToArrayAsync();
+        }
+	}
+}
+
